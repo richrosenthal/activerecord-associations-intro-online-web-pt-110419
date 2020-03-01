8afd75900a02005415a1f8446936954f6414e038
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-03-01 22:02:29 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,5 +1,5 @@
-class CreateGenres < ActiveRecord::Migration
-    def change
+class CreateGenres < ActiveRecord::Migration[4.2]
+  def change
     create_table :genres do |t|
       t.string :name
     end
